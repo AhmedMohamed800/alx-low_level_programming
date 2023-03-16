c39b5e0eb28d4a6ce3a1e9b15e853c728e8f98aa
= <=>
2023-03-16 17:10:35 +0000

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints a message
+ * Return: Return 0
+ */
+int main(void)
+{
+	printf("with proper grmmar, but the outcome is a piece of art,\n");
+	return (0);
+}
