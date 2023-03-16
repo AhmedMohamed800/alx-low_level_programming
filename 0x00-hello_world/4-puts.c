1556816632f4fb7daede25477a38b9601e738351
= <=>
2023-03-16 16:55:03 +0000

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-  char ch[52];
-  strcpy(ch,"\"Programming is like building a multilingual puzzle");
-  puts(ch);
-  return (0);
+/* This funtction prints a string */
+int main(void)
+{
+	char ch[52];
+	strcpy(ch, "\"Programming is like building a multilingual puzzle");
+	puts(ch);
+	return (0);
 }
