629c4bc881f72b971d0c5a6a473d63ff52e21710
= <=>
2023-03-16 19:12:19 +0000

    UNIX is basically

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - prints a string
+ * Return: Return 1
+ */
+int main(void)
+{
+	char *my_string = "and that piece of art is useful\"";
+	char *ant = "- Dora Korpar, 2015-10-19\n";
+
+	write(1, my_string, strlen(my_string));
+	write(1, ant, strlen(ant));
+	return (1);
+}
