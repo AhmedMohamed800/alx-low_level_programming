f2ffa39b3d1a7a84f08e25cd3c58b632fe20b33e
= <=>
2023-03-17 13:03:07 +0000

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the alpabet
+ * Return: Return 0
+ */
+int main(void)
+{
+	int ch = 48;
+
+	for (ch = 48; ch < 58; ch++)
+		if (ch == 57)
+		{
+			putchar(ch);
+		}
+		else
+		{
+			putchar(ch);
+			putchar(',');
+		}
+	putchar('\n');
+	return (0);
+}
