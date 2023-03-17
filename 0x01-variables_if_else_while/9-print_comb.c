00806238c75c5efbe606114102c3811f1cb03d9c
= <=>
2023-03-17 13:08:04 +0000

    Patience, p

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,14 @@ int main(void)
 	int ch = 48;
 
 	for (ch = 48; ch < 58; ch++)
-		if (ch == 57)
+	{
+		putchar(ch);
+		if (ch != 57)
 		{
-			putchar(ch);
-		}
-		else
-		{
-			putchar(ch);
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
