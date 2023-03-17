ad73ef642c35a319f104e47a68f347280930193c
= <=>
2023-03-17 18:05:49 +0000

    The success

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,20 +10,20 @@ int main(void)
 
 	for (num_1 = 48; num_1 <= 55; num_1++)
 	{
-        for (num_2 = num_1 + 1; num_2 <= 56; num_2++)
-        {
-            for (num_3 = num_2 + 1; num_3 <= 57; num_3++)
+		for (num_2 = num_1 + 1; num_2 <= 56; num_2++)
 		{
-			putchar(num_1);
-			putchar(num_2);
-            putchar(num_3);
-			if (num_1 != 55 || num_2 != 56 || num_3 != 57)
+			for (num_3 = num_2 + 1; num_3 <= 57; num_3++)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(num_1);
+				putchar(num_2);
+				putchar(num_3);
+				if (num_1 != 55 || num_2 != 56 || num_3 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
-    }
 	}
 	putchar('\n');
 	return (0);
