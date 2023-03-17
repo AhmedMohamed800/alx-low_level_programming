29ddb0c75b6d26d76c1c78c75ea503b2bb59f5df
= <=>
2023-03-17 17:48:56 +0000

    Inventing is a combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,19 +4,23 @@
  * main - print all possible different combinations of two digits
  * Return: Return (0)
  */
-int main() 
+int main(void)
 {
-    int num_1, num_2;
+	int num_1, num_2;
 
-    for(num_1 = 48; num_1 <= 56; num_1++) {
-        for(num_2 = num_1 + 1; num_2 <= 57; num_2++) {
-            putchar(num_1);
-            putchar(num_2);
-            if(num_1 != 56 || num_2 != 57) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    return 0;
+	for (num_1 = 48; num_1 <= 56; num_1++)
+	{
+		for (num_2 = num_1 + 1; num_2 <= 57; num_2++)
+		{
+			putchar(num_1);
+			putchar(num_2);
+			if (num_1 != 56 || num_2 != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
