9263244541c5878468a20ad3c799a05e2d67c7f9
= <=>
2023-03-17 17:40:26 +0000

    test

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,28 @@
  */
 int main(void)
 {
-	int num_1 = 48, num_2 = 49;
+	int num_1 = 0, num_2 = 0;
 
-	while (!(num_1 == 57 && num_2 == 48))
+	while (!(num_1 == 9 && num_2 == 9))
 	{
-		if (!(num_1 == num_2))
+		if (num_1 < num_2)
 		{
-			putchar(num_1);
-			putchar(num_2);
-			num_2++;
+			putchar(num_1 + '0');
+			putchar(num_2 + '0');
 		}
-		if (!(num_1 == 57 && num_2 == 48))
+		if (!(num_1 == 8 && num_2 == 9))
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		if (num_2 == 57)
+		if (num_2 == 9)
 		{
 			num_1++;
-			num_2 = 1;
+			num_2 = num_1 + 1;
+		}
+		else
+		{
+			num_2++;
 		}
 
 	}
