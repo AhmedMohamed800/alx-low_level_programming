07960cac8201277320fd6391dcda95bebc4b5db0
= <=>
2023-03-21 17:00:13 +0000

    day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints the value of the last digit
+ * Return: Return nothing
+ */
+int jack_bauer(void)
+{
+	int first = 0, second = 0, third = 0, forth = 0;
+
+	while (first != 2 && second != 3 && third = 6 && forth = 0)
+	{
+		if (forth < 9)
+		{
+			forth += 1;
+		} else if (forth == 9)
+		{
+			forth = 0;
+			third += 1;
+		}
+		if (third == 6 && forth == 0 && second != 9)
+		{
+			third = 0;
+			forth = 0;
+			second += 1
+		}
+		else
+		{
+			first += 1;
+			second = 0;
+			third = 0;
+			forth = 0;
+		}
+	}
+
+}
