20149358fa1889f4501c270c3bf7c3c11a9f82c5
= <=>
2023-03-21 16:28:02 +0000

    Make a function that print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints the value of the last digit
+ * @n: an integer
+ * Return: Return the last digit
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		return ((n % 10) * -1);
+	}
+	return (n % 10);
+}
