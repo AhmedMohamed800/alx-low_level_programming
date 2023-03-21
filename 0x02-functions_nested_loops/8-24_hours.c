4588d4a9f0a0b14f5269e39643ee46cb33b3b8fd
= <=>
2023-03-21 17:28:34 +0000

    go

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,12 @@ void jack_bauer(void)
 
 	for (hour = 0; hour < 24; hour++)
 	{
-		for (min = 0; min < 59; min++)
+		for (min = 0; min < 60; min++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((hour / 10) + '0');
+			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
