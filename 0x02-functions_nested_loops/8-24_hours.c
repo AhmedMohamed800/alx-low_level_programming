d6f845a8f34e0f53c24b23c0c54623868c7700b7
= <=>
2023-03-21 17:25:47 +0000

    go

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,41 +2,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints the value of the last digit
+ * jack_bauer - prints min and hours
  * Return: Return nothing
  */
 void jack_bauer(void)
 {
-	int first = 0, second = 0, third = 0, forth = 0;
+	int hour, min;
 
-	while (first != 2 && second != 3 && third != 5 && forth != 9)
+	for (hour = 0; hour < 24; hour++)
 	{
-		putchar(first);
-		putchar(second);
-		putchar(':');
-		putchar(third);
-		putchar(forth);
-		if (forth < 9)
+		for (min = 0; min < 60; min++)
 		{
-			forth += 1;
-		} else if (forth == 9)
-		{
-			forth = 0;
-			third += 1;
-		}
-		if (third == 6 && forth == 0 && second != 9)
-		{
-			third = 0;
-			forth = 0;
-			second += 1;
-		}
-		else if (second == 9 && third == 6 && forth == 0)
-		{
-			first += 1;
-			second = 0;
-			third = 0;
-			forth = 0;
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((hour / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
 		}
 	}
-
 }
+
