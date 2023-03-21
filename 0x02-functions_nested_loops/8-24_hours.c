75be7b19549d80aaae655349de1fa2e1e84d2391
= <=>
2023-03-21 17:20:15 +0000

    testo1213

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 	while (first != 2 && second != 3 && third != 5 && forth != 9)
 	{
 		putchar(first);
-		putchar(second;
+		putchar(second);
 		putchar(':');
 		putchar(third);
 		putchar(forth);
