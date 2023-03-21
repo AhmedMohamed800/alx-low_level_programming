e1e5a5066b08f4756ba23a4f9b90a627abbe4d09
= <=>
2023-03-21 18:41:57 +0000

    sd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @n: first integer
+ * @f: second integer
+ * Return: Return the sum of two number
+ */
+int add(int n, int f)
+{
+	return (n + f);
+}
