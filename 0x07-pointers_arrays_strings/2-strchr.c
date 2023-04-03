8a34002d042c871577acf1ac6dc605937b10c6ce
= <=>
2023-04-03 10:02:31 +0000

    test

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,13 @@
 */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			s = s + i;
 			return (s);
 		}
+		s++;
 	}
 	return ('\0');
 }
