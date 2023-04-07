36f578b3d4da3b749cb1fec838c929e41d6bb665
= <=>
2023-04-07 09:02:33 +0000

    fix a bug

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strcmp - concatenates two strings
+* @s1: the first string
+* @s2: the second string
+* Return: (s1 < s2 - s1 > s2 - s1 = s2) less - equal - >0
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	return (*s1 - *s2);
+}
