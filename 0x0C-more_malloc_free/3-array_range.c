884dd181b6e32aa52898a974c93c6c42828dba6c
= <=>
2023-04-13 12:10:26 +0000

    Add array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* array_range - creates an array of integers.
+* @min: point of start.
+* @max: point of end.
+* Return: pointer to the newly created array.
+*/
+int *array_range(int min, int max)
+{
+	int *arr, i, sum = max - min;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	arr = malloc(4 * sum);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < sum; i++)
+	{
+		arr[i] = min;
+		min++;
+	}
+	return (arr);
+}
