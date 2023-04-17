c6905a84904ac44b6ba47c909f9d09d0529795b6
= <=>
2023-04-17 09:51:27 +0000

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef __HEADER__
+#define __HEADER__
+
+/**
+* struct dog - contains doges information
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
