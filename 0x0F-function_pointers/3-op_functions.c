2e9d9df12381ffd31c6758ac0ee69f153dc244a2
= <=>
2023-04-19 12:17:19 +0000

    Add 3 files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+* op_add - returns the sum of a and b
+* @a: integer (1)
+* @b: integer (2)
+* Return: sum of a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - returns the difference of a and b
+* @a: integer (1)
+* @b: integer (2)
+* Return: difference of a and b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - returns the product of a and b.
+* @a: integer (1)
+* @b: integer (2)
+* Return: the product of a and b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - returns the result of the division of a by b.
+* @a: integer (1)
+* @b: integer (2)
+* Return: the result of the division of a by b.
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - returns the remainder of the division
+* @a: integer (1)
+* @b: integer (2)
+* Return: the remainder of the division of a by b.
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
