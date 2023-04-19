e7de3a17b72957fa7d1c04257da5267be42d2d85
= <=>
2023-04-19 09:00:29 +0000

    fix a bug with 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -10,9 +9,9 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
+	if (name == NULL || f == NULL)
+	{
 		return;
-	if (f == NULL)
-		return;
+	}
 	f(name);
 }
