e35125caba2997b5f2a17067281ffdf718106d8d
= <=>
2023-04-19 10:03:03 +0000

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,9 +6,26 @@
 * @array: array.
 * @size: array's size.
 * @cmp: function.
-* Return: ((-1, size <= 0), no matches) (index_of_element
+* Return: ((-1, size <= 0), no matches) (index_of_element)
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (!array && !cmp)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
