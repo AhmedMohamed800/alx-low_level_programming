2ace44eff65e28b70e513e884d1e47702a884c1e
= <=>
2023-04-20 09:47:51 +0000

    Add print_number

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints numbers
+* @separator: string to be printed between numbers
+* Return: nothing
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	char *separ;
+	unsigned int i;
+	va_list (args);
+
+	if (separator == NULL || *separator == 0)
+	{
+		separ = "";
+	}
+	else
+	{
+		separ = (char *) separator;
+	}
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d", va_arg(args, const unsigned int));
+		}
+		else
+		{
+			printf("%d%s", va_arg(args, const unsigned int), separ);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
