412615328bf8f21e6d2a927464f70583236970d5
= <=>
2023-04-27 15:58:27 +0000

    Add free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_list - frees a list_t list.
+* @head: pointer to head
+*	 Return: nothing
+*/
+void free_list(list_t *head)
+{
+	list_t *p = head, *next;
+
+	while (p->next != NULL)
+	{
+		next = p->next;
+		free(p);
+		p = next;
+	}
+}
