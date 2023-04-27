ed132bce9cb7f1b1c51cbef7841cffe899606a3d
= <=>
2023-04-27 14:22:39 +0000

    Add 0x12_linked_lists

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef __LISTS__
+#define __LISTS__
+
+/**
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}
+
+int _putchar(char c);
+
+#endif
