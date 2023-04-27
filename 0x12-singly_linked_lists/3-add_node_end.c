01726517ff8610a18d0c945084c87f5f0372e260
= <=>
2023-04-27 15:30:07 +0000

    Add add_node_end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* add_node_end - adds a new node at the end of a list_t list.
+* @head: pointer to head
+* @str: string to be duplicated
+* Return: the new node or NULL if it failed
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *p;
+	
+	p = *head;
+	if (new_node != NULL)
+	{
+		new_node->str = strdup(str);
+		new_node->len = strlen(str);
+		new_node->next = NULL;
+		while (p != NULL)
+		{
+			p = p->next;
+		}
+		p->next = new_node;
+	}
+	return (new_node);
+}
