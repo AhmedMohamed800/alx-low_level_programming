6221f83ee7235ed4f9527ba39c01b4616f0537e6
= <=>
2023-04-30 15:48:31 +0000

    Add 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* sum_listint - returns the sum of all the data (n) of a listint_t linked list
+* @head: pointer to head
+* return: sum of all data of linked list
+*/
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr = head;
+	int sum = 0;
+
+	while (ptr != NULL)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
