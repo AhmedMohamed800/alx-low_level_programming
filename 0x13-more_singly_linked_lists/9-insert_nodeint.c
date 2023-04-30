13670dfa6f1b7fb64008e6f7279bb937b4320fb7
= <=>
2023-04-30 17:55:43 +0000

    fix a bug with 9th task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,13 +9,13 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current = *head;
-	listint_t *last;
-	listint_t *new_node;
+	listint_t *current = *head, *last, *new_node;
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL || *head == NULL || (*head)->next == NULL)
+	if (new_node == NULL)
+		return (NULL);
+	if (current == NULL || current->next == NULL)
 		return (NULL);
 	new_node->n = n;
 	if (idx == 0)
