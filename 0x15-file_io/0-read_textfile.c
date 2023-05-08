1266a7f7a500fe89ae804fa4bc8ce6cf867e6859
= <=>
2023-05-08 10:15:16 +0000

    fix a betty bug with first task 0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int my_file;
 	char *buffer;
-	ssize_t read_file,writ;
+	ssize_t read_file, writ;
 
 	if (filename == NULL)
 		return (0);
