ceb3d5501018eeda6701e7c207d7dd5c5b04543e
= <=>
2023-05-08 10:09:26 +0000

    Add 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* read-textfile - reads a text file and prints it to the POSIX standard output.
+* @filename: the file that should be printed.
+* @letters: the number of letters it should read and print.
+* Return: the actual number of letters it could read and print.
+* On error, return 0 of file is NULL
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int my_file;
+	char *buffer;
+	size_t count = 0;
+
+	my_file = open(filename, O_RDONLY);
+	if (my_file == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(letters);
+	read(my_file, buffer, letters);
+	while (*buffer)
+	{
+		_putchar(*buffer);
+		buffer++;
+		count++;
+	}
+	buffer[count] = '\0';
+	close(my_file);
+	return (count);
+}
