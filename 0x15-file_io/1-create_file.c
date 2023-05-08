9d9774058c12487a4548d2d1fb708467a75f03eb
= <=>
2023-05-08 10:48:41 +0000

    bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,10 @@ int create_file(const char *filename, char *text_content)
 	int my_file;
 	size_t length = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	my_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (my_file == -1)
 		return (-1);
