0acbbb20f7d40a46d67196186a5e8ee6f80bd1b0
Ahmed Mohamed <=>
2023-06-15 18:14:18 +0000

    test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			current->prev = new;
 			return (new);
 		}
+		if (i == idx)
+			break;
 		current = current->next;
 		i++;
 	}
