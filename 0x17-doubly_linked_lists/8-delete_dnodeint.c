d1f51bd615cd0992dfec5e4535750aaed62e06ec
Ahmed Mohamed <=>
2023-06-15 19:03:32 +0000

    fix a bug

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,35 +12,26 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	if (index < 0 || index > 50)
-		return (-1);
-	if (index == 0)
+	for (; index != 0; index--)
 	{
-		if (current->next == NULL)
-		{
-			free(current);
-			*head = NULL;
-			return (1);
-		}
-		current->next->prev = NULL;
-		*head = current->next;
-		free(current);
-		return (1);
-	}
-	for (; index != 1; index--)
-	{
-		current = current->next;
 		if (current == NULL)
 			return (-1);
+		current = current->next;
 	}
-	if (current->next == NULL)
+	if (current != NULL)
 	{
-		current->prev->next = NULL;
+		if (current->prev != NULL)
+		{
+			current->prev->next = current->next;
+		}
+		else
+		{
+			*head = current->next;
+		}
+		if (current->next != NULL)
+			current->next->prev = current->prev;
 		free(current);
 		return (1);
 	}
-	current->next->prev = current->prev;
-	current->prev->next = current->next;
-	free(current);
-	return (1);
+	return (-1);
 }
