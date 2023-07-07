d862cd406923994298914da96227ab3905e5fbb3
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-07-07 07:05:09 +0000

    Add  3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,7 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				free(new_node);
 				return (1);
 			}
-			new_node = new_node->next;
+			current_node = current_node->next;
 		}
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
@@ -54,7 +54,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  */
 hash_node_t *create_node(const char *key, const char *value)
 {
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node = malloc(sizeof(hash_node_t *));
 
 	if (new_node == NULL)
 		return (NULL);
